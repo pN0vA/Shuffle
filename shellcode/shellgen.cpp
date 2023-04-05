fd55143ac863f062eb3f7ada02838b035ef05ad2
n0vA <58831441+pN0vA@users.noreply.github.com>
2023-04-04 20:03:21 -0600

    Add files via upload

---
+++ shellcode/shellgen.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <errno.h>
+#include <sys/mman.h>
+#include <stdint.h>
+#include <signal.h>
+
+#define SHELLCODE_MAX_SIZE 4096
+
+void handle_sigsegv(int sig) {
+    std::cerr << "Segmentation fault: Invalid memory access\n";
+    exit(1);
+}
+
+int main(int argc, char** argv) {
+
+    // Check for string argument
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <string>\n";
+        return 1;
+    }
+
+    // Get the string argument
+    char* input = argv[1];
+    size_t input_length = strlen(input);
+
+    // Allocate memory for shellcode buffer
+    char* shellcode = (char*)malloc(SHELLCODE_MAX_SIZE);
+    if (shellcode == NULL) {
+        std::cerr << "Failed to allocate memory for shellcode: " << strerror(errno) << "\n";
+        return 1;
+    }
+
+    // Convert string to shellcode
+    size_t shellcode_size = 0;
+    for (int i = 0; i < input_length && shellcode_size < SHELLCODE_MAX_SIZE; i++) {
+        shellcode[shellcode_size++] = input[i] ^ 0xff; // XOR with 0xff to avoid null bytes
+    }
+
+    // Print shellcode
+    std::cout << "Shellcode: ";
+    for (int i = 0; i < shellcode_size; i++) {
+        printf("\\x%02x", (unsigned char)shellcode[i]);
+    }
+    std::cout << "\n";
+
+    // Allocate executable memory
+    void* exec_mem = mmap(0, shellcode_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (exec_mem == MAP_FAILED) {
+        std::cerr << "Failed to allocate executable memory: " << strerror(errno) << "\n";
+        free(shellcode);
+        return 1;
+    }
+
+    // Copy shellcode to executable memory
+    memcpy(exec_mem, shellcode, shellcode_size);
+
+    // Calculate the return address
+    void* ret_addr = (void*)((uintptr_t)exec_mem + shellcode_size);
+
+    // Set up SIGSEGV handler to catch invalid memory access
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = handle_sigsegv;
+    if (sigaction(SIGSEGV, &sa, NULL) == -1) {
+        std::cerr << "Failed to set up signal handler: " << strerror(errno) << "\n";
+        munmap(exec_mem, shellcode_size);
+        free(shellcode);
+        return 1;
+    }
+
+    // Set the return address
+    void (*code)() = (void(*)())exec_mem;
+    asm("mov %0, %%rsp; ret" : : "r"(ret_addr));
+    code();
+
+    // Free memory
+    munmap(exec_mem, shellcode_size);
+    free(shellcode);
+
+    return 0;
+}
