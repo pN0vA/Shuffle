24214388ee88d9c6c27c6cc573631ef1160125f0
n0vA <58831441+pN0vA@users.noreply.github.com>
2023-04-08 19:20:16 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 sudo apt install libncurses5-dev libncursesw5-dev
 sudo apt install libcapstone-dev
 sudo apt-get install mingw-w64
+sudo apt install metasploit-framework
 sudo apt install cowsay
 
 cowsay INSTALL COMPLETE
