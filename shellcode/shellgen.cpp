ed24243ce0dea53a2d73fce7cbb41f5a69b665a3
n0vA <58831441+pN0vA@users.noreply.github.com>
2023-04-30 13:35:56 -0600

    Add files via upload

--- shellcode/shellgen.cpp
+++ shellcode/shellgen.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
+#include <execinfo.h>
 #include <cstring>
 #include <cstdlib>
+#include <cstddef>
+#include <cstdio>
 #include <errno.h>
 #include <sys/mman.h>
 #include <stdint.h>
 #include <signal.h>
+#include <unistd.h>
 
 #define SHELLCODE_MAX_SIZE 4096
 
 void handle_sigsegv(int sig) {
-    std::cerr << "Segmentation fault: Invalid memory access\n";
+    std::cerr << "Segmentation fault: Invalid memory access at address " << reinterpret_cast<void*>(static_cast<uintptr_t>(sig)) << "\n";
+
+    // get the backtrace and print it to STDERR
+    const int max_backtrace_frames = 10;
+    void* array[max_backtrace_frames];
+    size_t size = backtrace(array, max_backtrace_frames);
+    backtrace_symbols_fd(array, size, STDERR_FILENO);
+
     exit(1);
 }
 
@@ -46,7 +57,7 @@ int main(int argc, char** argv) {
     std::cout << "\n";
 
     // Allocate executable memory
-    void* exec_mem = mmap(0, shellcode_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    void* exec_mem = mmap(0, shellcode_size + 4096, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS | MAP_EXECUTABLE, -1, 0);
     if (exec_mem == MAP_FAILED) {
         std::cerr << "Failed to allocate executable memory: " << strerror(errno) << "\n";
         free(shellcode);
@@ -65,19 +76,21 @@ int main(int argc, char** argv) {
     sa.sa_handler = handle_sigsegv;
     if (sigaction(SIGSEGV, &sa, NULL) == -1) {
         std::cerr << "Failed to set up signal handler: " << strerror(errno) << "\n";
-        munmap(exec_mem, shellcode_size);
+        munmap(exec_mem, shellcode_size + 4096);
         free(shellcode);
         return 1;
     }
 
+
     // Set the return address
     void (*code)() = (void(*)())exec_mem;
-    asm("mov %0, %%rsp; ret" : : "r"(ret_addr));
+    asm("mov %0, %%rsp; jmp *%1" : : "r"((char*)ret_addr - 8), "r"(ret_addr));
+
+    // Execute the shellcode
     code();
 
     // Free memory
-    munmap(exec_mem, shellcode_size);
+    munmap(exec_mem, shellcode_size + 4096);
     free(shellcode);
-
     return 0;
 }
